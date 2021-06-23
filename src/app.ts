5751a94eca215a502d6c1e7092081c1521b7b360
TaliaBeltran <taliabeltran.123@gmail.com>
2021-06-22 21:11:48 -0700

    modificando app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import express, { Express } from "express";
 import * as bodyParser from "body-parser";
 import UserModules from "./modules/usermodule/init";
-<<<<<<< HEAD
 import ClientModule from "./modules/clientsmodels/init";
 import PedidoModule from "./modules/pedidosmodule/init";
-=======
-import ClientModule from "./modules/clientesmodels/init";
-//import PedidoModule from "./modules/pedidosmodule/init";
->>>>>>> c0265398a82db6928a686fe05e1d56ee3faf283d
 import mongoose, { Mongoose } from "mongoose";
 import FileUpload from "express-fileupload";
 class App {
@@ -19,11 +14,7 @@ class App {
     this.initApp();
   }
   public connectDatabase() {
-<<<<<<< HEAD
-    let host: string = "mongodb://172.26.0.2:27017";
-=======
-    let host: string = "mongodb://172.19.0.2:27017";
->>>>>>> c0265398a82db6928a686fe05e1d56ee3faf283d
+    let host: string = "mongodb://172.20.0.2:27017";
     let database: string = process.env.DATABASE || "seminario";
     let connectionString: string = `${host}/${database}`;
     mongoose.connect(connectionString, {
@@ -49,11 +40,7 @@ class App {
     console.log("LOAD MODULES");
     const userModule = new UserModules("/api", this.app);
     const clientModule = new ClientModule("/client", this.app);
-<<<<<<< HEAD
     const pedidoModule = new PedidoModule("/pedido", this.app);
-=======
-    //const pedidoModule = new PedidoModule("/pedido", this.app);
->>>>>>> c0265398a82db6928a686fe05e1d56ee3faf283d
   }
 }
 export default new App();
